673f554ded8fcb03227fa147360949c957401e41
quangtung268 <72648545+quangtung268@users.noreply.github.com>
2022-06-18 00:33:01 +0700

    Add files via upload
    TSN model

---
+++ model.py
@@ -0,0 +1,169 @@
+import torch.nn
+import torch.nn as nn
+from transform import *
+from utils import *
+
+
+class TSN(nn.Module):
+    def __init__(self, num_class, num_segments, modality, base_model='resnet101',
+                 new_length=None, consensus_type='avg', before_softmax=True,
+                 dropout=0.8, crop_num=1, partial_bn=True):
+        super(TSN, self).__init__()
+        self.num_class = num_class
+        self.num_segments = num_segments
+        self.modality = modality
+        self.consensus_type = consensus_type
+        self.dropout = dropout
+        self.crop_num = crop_num
+        self.partial_bn = partial_bn
+
+        if self.new_length is None:
+            self.new_length = 1 if modality == 'RGB' else 5
+        else:
+            self.new_length = new_length
+
+        print((f"""Initializing TSN with base model: {base_model}.
+                    TSN Configurations:
+                    input_modality:     {self.modality}
+                    num_segments:       {self.num_segments}
+                    new_length:         {self.new_length}
+                    consensus_module:   {self.consensus_type}
+                    dropout_ratio:      {self.dropout}"""))
+
+        self._prepare_base_model(base_model)
+        feature_dim = self._prepare_tsn(num_class)
+        self.consensus = ConsensusModule(consensus_type)
+        if not before_softmax:
+            self.softmax = nn.Softmax()
+
+        self._enable_pbn = partial_bn
+        if partial_bn:
+            self.partialBN(True)
+
+    def _prepare_tsn(self, num_class):
+        feature_dim = getattr(self.base_model, self.base_model.last_layer_name).in_features
+        if self.dropout == 0:
+            setattr(self.base_model, self.base_model.last_layer_name, nn.Linear(feature_dim, num_class))
+            self.new_fc = None
+        else:
+            setattr(self.base_model, self.base_model.last_layer_name, nn.Dropout(p=self.dropout))
+            self.new_fc = nn.Linear(feature_dim, num_class)
+
+        std = 0.001
+        if self.fc is None:
+            nn.init.normal(getattr(self.base_model, self.base_model.last_layer_name).weight, 0, std)
+            nn.init.constant(getattr(self.base_model, self.base_model.last_layer_name).bias, 0)
+        else:
+            nn.init.normal(self.new_fc.weight, 0, std)
+            nn.init.constant(self.new_fc.bias, 0)
+
+        return feature_dim
+
+    def _prepare_base_model(self, base_model):
+        if 'resnet' in base_model or 'vgg' in base_model:
+            self.base_model = getattr(torchvision.models, base_model)(True)
+            self.base_model.last_layer_name = 'fc'
+            self.input_size = 224
+            self.input_mean = [0.485, 0.456, 0.406]
+            self.input_std = [0.229, 0.224, 0.225]
+
+        else:
+            raise ValueError('Unknown base model: {}'.format(base_model))
+
+    def train(self, mode=True):
+        """
+        Override the default train() to freeze the BN parameters
+        """
+        super(TSN, self).__init__()
+        count = 0
+        if self._enable_pbn:
+            print("Freezing BatchNorm2D except the first one.")
+            for m in self.base_model.modules():
+                if isinstance(m, nn.BatchNorm2d):
+                    count += 1
+                    if count >= (2 if self._enable_pbn else 1):
+                        m.eval()
+                        m.weight.requires_grad = False
+                        m.bias.requires_grad = False
+
+    def partialBN(self, enable):
+        self._enable_pbn = enable
+
+    def get_optim_policies(self):
+        first_conv_weight = []
+        first_conv_bias = []
+        normal_weight = []
+        normal_bias = []
+        bn = []
+
+        conv_cnt = 0
+        bn_cnt = 0
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, torch.nn.Conv1d):
+                ps = list(m.parameters())
+                conv_cnt += 1
+                if conv_cnt == 1:
+                    first_conv_weight.append(ps[0])
+                    if len(ps) == 2:
+                        first_conv_bias.append(ps[1])
+                else:
+                    normal_weight.append(ps[0])
+                    if len(ps) == 2:
+                        normal_bias.append(ps[1])
+
+            elif isinstance(m, nn.BatchNorm1d):
+                bn.extend(list(m.parameters()))
+            elif isinstance(m, nn.BatchNorm2d):
+                bn_cnt += 1
+                # later BN's are frozen
+                if not self._enable_pbn or bn_cnt == 1:
+                    bn.extend(list(m.parameters()))
+            elif len(m._modules) == 0:
+                if len(list(m.parameters())) > 0:
+                    raise ValueError("New atomic module type: {}. Need to give it a learning policy".format(type(m)))
+
+        return [
+            {'params': first_conv_weight, 'lr_mult': 5 if self.modality == 'Flow' else 1, 'decay_mult': 1,
+             'name': "first_conv_weight"},
+            {'params': first_conv_bias, 'lr_mult': 10 if self.modality == 'Flow' else 2, 'decay_mult': 0,
+             'name': "first_conv_bias"},
+            {'params': normal_weight, 'lr_mult': 1, 'decay_mult': 1,
+             'name': "normal_weight"},
+            {'params': normal_bias, 'lr_mult': 2, 'decay_mult': 0,
+             'name': "normal_bias"},
+            {'params': bn, 'lr_mult': 1, 'decay_mult': 0,
+             'name': "BN scale/shift"},
+        ]
+
+    def forward(self, x):
+        sample_len = 3 * self.new_length
+        base_out = self.base_model(input.view((-1, sample_len) + input.size()[2:]))
+
+        if self.dropout > 0:
+            base_out = self.new_fc(base_out)
+        if not self.before_softmax:
+            base_out = self.softmax(base_out)
+        if self.reshape:
+            base_out = base_out.view((-1, self.num_segments) + base_out.size()[1:])
+
+        output = self.consensus(base_out)
+        return output.squeeze(-1)
+
+    @property
+    def crop_size(self):
+        return self.input_size
+
+    @property
+    def scale_size(self):
+        return self.input_size * 256 // 224
+
+    def get_augmentation(self):
+        if self.modality == 'RGB':
+            return torchvision.transforms.Compose([GroupMultiScaleCrop(self.input_size, [1, .875, .75, .66]),
+                                                   GroupRandomHorizontalFlip(is_flow=False)])
+        elif self.modality == 'Flow':
+            return torchvision.transforms.Compose([GroupMultiScaleCrop(self.input_size, [1, .875, .75]),
+                                                   GroupRandomHorizontalFlip(is_flow=True)])
+        elif self.modality == 'RGBDiff':
+            return torchvision.transforms.Compose([GroupMultiScaleCrop(self.input_size, [1, .875, .75]),
+                                                   GroupRandomHorizontalFlip(is_flow=False)])
